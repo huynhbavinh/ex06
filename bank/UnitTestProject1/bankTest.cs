84c1f2fdaaef214467b8cbc8d8c43c583185146f
Nguyễn Thanh Duy Lân <lannguyenthanhduy@gmail.com>
2019-10-15 15:30:10 +0700

    15/10/2019

---
+++ bank/UnitTestProject1/bankTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using bank;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class bankTest
+    {
+        [TestMethod]
+        public void TestMethodBankAccount()
+        {
+            BankAccount acc1 = new BankAccount(2000);
+
+            Assert.IsNotNull(acc1);
+            Assert.IsInstanceOfType(acc1, typeof(BankAccount));
+            Assert.AreEqual(0, acc1.balance());
+        }
+    }
+}
---
+++ bank/bank/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    public class BankAccount
+    {
+        private int amount;
+        private int wallet;
+        public BankAccount(int money)
+        {
+            amount = money;
+            money = wallet;
+        }
+        public void deposit(int money)
+        {
+            amount += money;
+        }
+        public static void withDraw(int amount)
+        {
+
+        }
+        public static int balance(int amount)
+        { 
+            return amount;
+        }
+        public static void Main(String[] args)
+        {
+
+        }
+    }
+}
